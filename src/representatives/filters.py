04b90922f9c165f9799320bf4cbb5a25974dd6af
okhin <okhin@okhin.fr>
2017-01-16 11:48:08 +0100

    Fixing the non PEP8 compliant lines

--- src/representatives/filters.py
+++ src/representatives/filters.py
@@ -16,19 +16,20 @@ class ActiveMandateQueryFilterBackend(BaseFilterBackend):
     the parameter used in the query to filter is, by default mandates_active
     and it list a list of mandates among which one should be active.
     """
-    query_param = getattr(settings, 'ACTIVE_MANDATES_QUERY_PARAM', 'active_mandates')
+    query_param = getattr(settings, 'ACTIVE_MANDATES_PARAM', 'active_mandates')
 
     def filter_queryset(self, request, queryset, view):
         qs = queryset
 
         if self.query_param in request.GET:
             if len(request.GET[self.query_param]):
+                # We want to check for params in a list of mandates
+                mandates = request.GET[self.query_params].split(',')
                 qs = qs.filter(mandates__in=Mandate.objects.filter(
-                    Q(end_date__gte=datetime.today)
-                    | Q(end_date__isnull=True)).filter(
-                        Q(group__name=request.GET[self.query_param])
-                        | Q(group__abbreviation=request.GET[self.query_param])
-                        )
-                    ).distinct()
+                    Q(end_date__gte=datetime.today) |
+                    Q(end_date__isnull=True)).filter(
+                        Q(group__name__in=mandates) |
+                        Q(group__abbreviation__in=mandates)
+                )).distinct()
                 return qs
         return qs
