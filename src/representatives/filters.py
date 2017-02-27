9f75dfea1c83876d05e6efdc9e7d50948b233739
okhin <okhin@okhin.fr>
2017-02-27 12:35:22 +0100

    Stupid typo

--- src/representatives/filters.py
+++ src/representatives/filters.py
@@ -50,7 +50,7 @@ class ActiveConstituencyFilterBackend(BaseFilterBackend):
 
         if self.query_param in request.GET:
             if len(request.GET[self.query_params]):
-                mandates = urlunqoute(request.GET[self.query_param]).split(',')
+                mandates = urlunquote(request.GET[self.query_param]).split(',')
                 qs = qs.filter(mandates__in=Mandate.objects.filter(
                     Q(end_date__gte=datetime.today) |
                     Q(end_date__isnull=True)).filter(
